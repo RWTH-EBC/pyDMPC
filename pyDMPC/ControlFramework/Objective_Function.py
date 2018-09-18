a2f21d1c596260d65c866e874899df0d922d9408
Marc Baranski <mbaranski@eonerc.rwth-aachen.de>
2018-09-19 00:41:52 +0200

    * Correct the NC-DMPC implementation, #9 - Comment the bruto-force algorithm - Include iteration number in the subsystem - Wrong temperature conversions

--- pyDMPC/ControlFramework/Objective_Function.py
+++ pyDMPC/ControlFramework/Objective_Function.py
@@ -14,12 +14,12 @@ gl_res_grid = np.zeros([2,1])
 
 
 def TranslateModel(model_path, name, position):
-    global gl_model_compiled    
+    global gl_model_compiled
     if  gl_model_compiled[position-1] == False:
         import os
         import sys
         global dymola
-        
+
         if dymola is None:
             # Work-around for the environment variable
             sys.path.insert(0, os.path.join('C:\\',
@@ -29,20 +29,20 @@ def TranslateModel(model_path, name, position):
                                             'Library',
                                             'python_interface',
                                             'dymola.egg'))
-            
-            
+
+
             # Import Dymola Package
             from dymola.dymola_interface import DymolaInterface
-            
+
             # Start the interface
             dymola = DymolaInterface()
-        
+
         """ Simulation """
-        # Open dymola library 
+        # Open dymola library
         for lib in Init.path_lib:
             check1 = dymola.openModel(os.path.join(lib,'package.mo'))
             print("Opening successful" + str(check1))
-    
+
         dymola.ExecuteCommand("Advanced.CompileWith64=2")
         dymola.cd(Init.path_res +'\\' + name)
         # Translate the model
@@ -50,48 +50,48 @@ def TranslateModel(model_path, name, position):
         print("Translation successful" + str(check2))
         if check2 is True:
             gl_model_compiled[position-1] = True
-            
 
-def Obj(values_DVs, BC, name, model_path, position, output_vars, initial_names, initial_values):  
-    
+
+def Obj(values_DVs, BC, name, model_path, position, output_vars, initial_names, initial_values):
+
     import Init
     import os
     from modelicares import SimRes
     import numpy as np
-    
+
     import scipy.io as sio
-    
-    
+
+
     """ Simulation """
-    # Open dymola library 
+    # Open dymola library
     TranslateModel(model_path, name, position)
 
     global dymola
     obj_fnc_val = 'objectiveFunction'
-    
+
     """ Store two .mat-files that can be read by Dymola """
     #This file contains the input setting BC1/ BC2/..
     BC_array = np.empty([1,len(BC)+1])
     BC_array[0,0] = 0
     for i,val in enumerate(BC):
-        BC_array[0][i+1] = val  
+        BC_array[0][i+1] = val
     """This file contains the decision variable setting DV1/ DV2/..   """
     if isinstance (values_DVs, np.ndarray):
-        DV_array = np.empty([1,2])   
-        DV_array[0,0] = 0 
-        DV_array[0,1] = float(values_DVs) 
+        DV_array = np.empty([1,2])
+        DV_array[0,0] = 0
+        DV_array[0,1] = float(values_DVs)
     else:
-        DV_array = np.empty([1,len(values_DVs)+1])   
-        DV_array[0,0] = 0    
-        for i2,val2 in enumerate(values_DVs):    
-            DV_array[0][i2+1] = val2 
+        DV_array = np.empty([1,len(values_DVs)+1])
+        DV_array[0,0] = 0
+        for i2,val2 in enumerate(values_DVs):
+            DV_array[0][i2+1] = val2
 
-    
-    subsys_path = Init.path_res +'\\' + name 
-    sio.savemat((subsys_path +'\\'+ Init.fileName_DVsInputTable + '.mat'), {Init.tableName_DVsInputTable :DV_array})  
-    sio.savemat((subsys_path +'\\'+ Init.fileName_BCsInputTable + '.mat'), {Init.tableName_BCsInputTable :BC_array})  
-    
-    final_names = [obj_fnc_val]   
+
+    subsys_path = Init.path_res +'\\' + name
+    sio.savemat((subsys_path +'\\'+ Init.fileName_DVsInputTable + '.mat'), {Init.tableName_DVsInputTable :DV_array})
+    sio.savemat((subsys_path +'\\'+ Init.fileName_BCsInputTable + '.mat'), {Init.tableName_BCsInputTable :BC_array})
+
+    final_names = [obj_fnc_val]
 
 
     try:
@@ -120,8 +120,8 @@ def Obj(values_DVs, BC, name, model_path, position, output_vars, initial_names, 
                 #initialNames = initial_names,
                 #initialValues = initial_values,
             )
-        print('simulated' + simStat)  
-        
+        #print('simulated' + str(simStat))
+
         # Get the simulation result
         sim = SimRes(os.path.join(subsys_path, 'dsres.mat'))
     except:
@@ -150,11 +150,11 @@ def Obj(values_DVs, BC, name, model_path, position, output_vars, initial_names, 
                 #initialNames = initial_names,
                 #initialValues = initial_values,
             )
-        print(simStat)  
-        
+        #print(simStat)
+
         # Get the simulation result
         sim = SimRes(os.path.join(subsys_path, 'dsres.mat'))
-        
+
     # Penalty in case optimization algorithm does not support boundaries
     new_values_DVs = sim['decisionVariables.y[1]'].values()
     bounds = [0, 100]
@@ -163,11 +163,11 @@ def Obj(values_DVs, BC, name, model_path, position, output_vars, initial_names, 
         penalty_val = 10
     elif new_values_DVs[-1] < bounds[0]:
         penalty_val = 10
-        
+
     obj_fnc_vals = sim[obj_fnc_val].values()
     obj_fnc_vals = obj_fnc_vals + penalty_val
-    
-    #store output 
+
+    #store output
     output_list = []
     if output_vars is not None:
         for val in output_vars:
@@ -175,12 +175,12 @@ def Obj(values_DVs, BC, name, model_path, position, output_vars, initial_names, 
             output_list.append(output_vals[-1])
         global gl_output
         gl_output = output_list
-        
+
     """ Track Optimizer """
     global gl_res_grid
     step = np.array([[float(values_DVs)], [obj_fnc_vals[-1]]])
     gl_res_grid = np.append(gl_res_grid,step,axis = 1)
-       
+
     return obj_fnc_vals[-1]
 
 
@@ -188,27 +188,25 @@ def CloseDymola():
     global dymola
     if dymola is not None:
         dymola.close()
-        dymola = None  
+        dymola = None
         global gl_model_compiled
         gl_model_compiled = False
-        
+
 def ChangeDir(name):
     global dymola
     if dymola is not None:
         dymola.cd(Init.path_res +'\\' + name)
-    
+
 
 def GetOutputVars():
     global gl_output
     return gl_output
-        
+
 def GetOptTrack():
     global gl_res_grid
     gl_res_grid = np.delete(gl_res_grid,0,1)
     return gl_res_grid
-        
+
 def DelLastTrack():
     global gl_res_grid
     gl_res_grid =np.zeros([2,1])
-        
-    
--- pyDMPC/ControlFramework/Subsystem.py
+++ pyDMPC/ControlFramework/Subsystem.py
@@ -14,11 +14,11 @@ gl_commands_costs = []
 gl_measurements_all =[]
 
 class Subsystem():
-        
+
     def __init__(self, name, position,
-                 num_DVs,num_BCs, init_DecVars, sim_time, 
-                 bounds_DVs,model_path, names_BCs, 
-                   num_VarsOut, Id_BC1, Id_BC2, names_DVs, 
+                 num_DVs,num_BCs, init_DecVars, sim_time,
+                 bounds_DVs,model_path, names_BCs,
+                   num_VarsOut, Id_BC1, Id_BC2, names_DVs,
                    output_vars, initial_names, IDs_initial_values,
                    type_subSyst=None):
         self._name = name
@@ -35,7 +35,7 @@ class Subsystem():
         self.lookUpTables = None
         self._model_path = model_path
         self._names_BCs = names_BCs
-        
+
         self._Id_BC1 = Id_BC1 #T
         self._Id_BC2 = Id_BC2 #relHum + T_hum
         self._names_DVs = names_DVs
@@ -43,15 +43,15 @@ class Subsystem():
         self._initial_names = initial_names
         self._IDs_initial_values = IDs_initial_values
 
-        
+
     def GetNeighbour(self, neighbour_name):
         self.neighbour_name = neighbour_name
-        
+
     def GetMeasurements(self, ids_list):
 
         #read
         values = []
-        
+
         for val in range(1,20):
             values.append(25)
 
@@ -60,7 +60,7 @@ class Subsystem():
         values[6] = 0.005
         values[8] = 0.005
         values[17] = 0.005
-        
+
         measurementsAll = values
         self.measurements = np.array(values)
         self._initial_values = np.array(values)
@@ -71,16 +71,16 @@ class Subsystem():
 
         #Save new 'CompleteInput.mat' File
         sio.savemat((Init.path_res +'\\' + 'Inputs' +'\\' + 'CompleteInput.mat'), {'InputTable' :np.array(measurementsAll)})
-            
 
-        
 
-    """ Continue util Init.stop_time (experiment time) is reached """    
-    def CalcDVvalues(self, time_step, time_storage):     
+
+
+    """ Continue util Init.stop_time (experiment time) is reached """
+    def CalcDVvalues(self, time_step, time_storage, iter):
         """ Get Measurements """
         self.measurements = [0.005, 25]
         self._initial_values = [0.005, 25]
-        
+
         """ Import selected algorithm (and choose objective function) """
         if Init.algorithm == "BExMoC":
             import algorithm.BExMoC
@@ -89,14 +89,14 @@ class Subsystem():
             if time_step == 0:
                 if self.values_BCs is None:
                     self.values_BCs = BExMoC.CalcBCvalues(Init.amount_vals_BCs, Init.exp_BCs,
-                                                 Init.center_vals_BCs, Init.factors_BCs, Init.amount_lower_vals, Init.amount_upper_vals)   
+                                                 Init.center_vals_BCs, Init.factors_BCs, Init.amount_lower_vals, Init.amount_upper_vals)
 
-            """ Execute if new optimization is requested """ 
- 
+            """ Execute if new optimization is requested """
+
             """ Get initial_values for simulation """
             # Check if optimization phase is due
             if time_step-time_storage < Init.optimization_interval and time_step != 0:
-                """ Interpolation """ 
+                """ Interpolation """
                 try:
                     [commands, costs, outputs] = BExMoC.Interpolation(self.measurements, self.lookUpTables[1],
                                                 self._bounds_DVs, self.lookUpTables[0], self.lookUpTables[2])
@@ -104,25 +104,25 @@ class Subsystem():
                     commands = []
                     costs = []
                     outputs = []
-                            
+
                     for i in range(0,len(self.lookUpTables[1])):
                         commands.append(0)
                         costs.append(0)
                         outputs.append([0,0])
 
-                print(str(commands))
+                #print(str(commands))
 
             else:
-                time_storage = time_step # store the time 
+                time_storage = time_step # store the time
                 [storage_cost, storage_DV, storage_out, exDestArr, res_grid] = BExMoC.CalcLookUpTables(self, Init.obj_function, time_storage, Init.path_lib, Init.init_conds)
                 self.lookUpTables = [storage_cost, storage_DV, storage_out]
-                print(self.neighbour_name)
+                #print(self.neighbour_name)
                 """ Store look-up table for upstream subsystem in directory of upstream subsystem """
                 if exDestArr is not None and self.neighbour_name is not None:
-                    sio.savemat((Init.path_res +'\\' + self.neighbour_name +'\\' +  Init.fileName_Cost + '.mat'), {Init.tableName_Cost :exDestArr})   
+                    sio.savemat((Init.path_res +'\\' + self.neighbour_name +'\\' +  Init.fileName_Cost + '.mat'), {Init.tableName_Cost :exDestArr})
                 """Store optimizer results"""
-                sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'OptimizerTrack.mat' ), {'OptimizerTrackCounter11': res_grid})   
-                
+                sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'OptimizerTrack.mat' ), {'OptimizerTrackCounter11': res_grid})
+
                 try:
                     [commands, costs, outputs] = BExMoC.Interpolation(self.measurements, self.lookUpTables[1],
                                                 self._bounds_DVs, self.lookUpTables[0], self.lookUpTables[2])
@@ -130,115 +130,119 @@ class Subsystem():
                     commands = []
                     costs = []
                     outputs = []
-                            
+
                     for i in range(0,len(self.lookUpTables[1])):
                         commands.append(0)
                         costs.append(0)
                         outputs.append(0)
-                        
+
             if time_storage != time_step:
                 """ Store global commands""" # just for analysis
                 global gl_commands_costs
                 tz = pytz.timezone('Europe/Berlin')
                 ts = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
-                print(self.measurements)
-                print(outputs)
-                gl_commands_costs.append([np.array([[self.measurements[0]]]), np.array([[self.measurements[1]]]), commands, costs, np.array([[outputs[0][0]-273.15]]), np.array([[outputs[0][1]]]), self._name, ts])
+                #print(self.measurements)
+                #print(outputs)
+                gl_commands_costs.append([np.array([[self.measurements[0]]]), np.array([[self.measurements[1]]]), commands, costs, np.array([[outputs[0][0]]]), np.array([[outputs[0][1]]]), self._name, ts])
                 #gl_commands_costs.append([commands, costs, self._name, ts])
-                sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'CommandsCosts.mat' ), {'CommandsCosts': gl_commands_costs})     
+                sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'CommandsCosts.mat' ), {'CommandsCosts': gl_commands_costs})
                 if self._name != 'Steam_humidifier':
                     self.SendCommands(commands)
-            else: 
+            else:
                 commands = -1
-                
+
             return commands
         elif Init.algorithm == "NC_DMPC":
             import algorithm.NC_DMPC
             NC_DMPC = algorithm.NC_DMPC
             values_BCs = []
-            if time_step == 0 or self.neighbour_name is None:
-            #if time_storage == 0 or self.neighbour_name is None:
+
+            if iter == 0 or self.neighbour_name is None:
                 BC_1 = self.measurements[::-1]
                 #if len(BC_1) ==1:
                 BC_2 = [BC_1[0]*1.5, BC_1[1]+0.000005]
             else:
                 BC_dict = sio.loadmat(Init.path_res +'\\' + self.neighbour_name +'\\' +  Init.fileName_Output + '.mat')
                 arrayBC = BC_dict['output']
+                print(BC_dict)
                 #Sort Input Conditions because "exDestArr" must be strictly increaing  """
                 if len(arrayBC[1]) ==4:
-                    absHum_measurements1 = self.CalcXfromRH(arrayBC[1][3]*100, arrayBC[1][2]-273.15)
-                    absHum_measurements2 = self.CalcXfromRH(arrayBC[2][3]*100, arrayBC[2][2]-273.15)
-                    BC_1 = [arrayBC[1][2]-273.15, absHum_measurements1]
-                    BC_2 = [arrayBC[2][2]-273.15, absHum_measurements2]
+                    absHum_measurements1 = self.CalcXfromRH(arrayBC[1][3]*100, arrayBC[1][2])
+                    absHum_measurements2 = self.CalcXfromRH(arrayBC[2][3]*100, arrayBC[2][2])
+                    BC_1 = [arrayBC[1][2], absHum_measurements1]
+                    BC_2 = [arrayBC[2][2], absHum_measurements2]
                     if arrayBC[1][2] == arrayBC[2][2] or arrayBC[1][3] == arrayBC[2][3]:
-                        BC_2 = [val*1.1 for val in BC_1]      
+                        BC_2 = [val*1.1 for val in BC_1]
                 else:
                     if arrayBC[0][1] < arrayBC[1][1]:
                         values_BCs.append([arrayBC[0][1], arrayBC[1][1]])
                     elif arrayBC[0][1] > arrayBC[1][1]:
-                        values_BCs.append([arrayBC[1][1], arrayBC[0][1]]) 
-                    else: 
-                        values_BCs.append([arrayBC[1][1], arrayBC[0][1]*1.5])     
+                        values_BCs.append([arrayBC[1][1], arrayBC[0][1]])
+                    else:
+                        values_BCs.append([arrayBC[1][1], arrayBC[0][1]*1.5])
 
             for i in range(len(BC_1)):
                 if BC_1[i]<BC_2[i]:
-                    values_BCs.append([BC_1[i], BC_2[i]])  #[[BC_1[0], BC_2[0]]] 
+                    values_BCs.append([BC_1[i], BC_2[i]])  #[[BC_1[0], BC_2[0]]]
                 else:
-                    values_BCs.append([BC_2[i], BC_1[i]])  #[[BC_1[0], BC_2[0]]] 
+                    values_BCs.append([BC_2[i], BC_1[i]])  #[[BC_1[0], BC_2[0]]]
             self.values_BCs = values_BCs
-            if time_step == 0:
-                last_DV = Init.init_DVs[0]
-                """ Store last_DV in own directory """
-                sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'last_DV.mat'), {'last_DV': last_DV})
-                
-                self.values_BCs = values_BCs  
+            print(values_BCs)
+
+            #if time_step == 0:
+            last_DV = Init.init_DVs[0]
+            """ Store last_DV in own directory """
+            sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'last_DV.mat'), {'last_DV': last_DV})
+
+            self.values_BCs = values_BCs
             """ Load "last_DV" """
             last_DV_dict = sio.loadmat(Init.path_res + '\\' + self._name + '\\' + 'last_DV.mat')
             last_DV = last_DV_dict['last_DV']
             [storage_cost, storage_DV, storage_out, exDestArr, storage_grid]  = NC_DMPC.Iteration(self, time_step)
             """Store optimizer results"""
-            sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'OptimizerTrack.mat' ), {'OptimizerTrack': storage_grid})    
+            sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'OptimizerTrack.mat' ), {'OptimizerTrack': storage_grid})
             """ Load, determine and store new "last_DV" """
             new_last_DV = last_DV*Init.convex_factor+(1-Init.convex_factor)*storage_DV[0][2]
             sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'last_DV.mat'), {'last_DV': new_last_DV})
-            print(new_last_DV)
+            #print(new_last_DV)
             """ Store costs in neighbour's folder """
             if exDestArr is not None and self.neighbour_name is not None:
-                sio.savemat((Init.path_res +'\\' + self.neighbour_name +'\\' +  Init.fileName_Cost + '.mat'), {Init.tableName_Cost :exDestArr})  
-            """ Store output values in own directory """     
+                sio.savemat((Init.path_res +'\\' + self.neighbour_name +'\\' +  Init.fileName_Cost + '.mat'), {Init.tableName_Cost :exDestArr})
+            """ Store output values in own directory """
             sio.savemat((Init.path_res + '\\' + self._name + '\\' + Init.fileName_Output + '.mat'), {Init.tableName_Output: storage_out})
-            """ Store costs in own directory for evaluation only"""  
+            """ Store costs in own directory for evaluation only"""
             sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'Costs.mat'), {Init.tableName_Output: storage_cost})
             commands = float(new_last_DV)
             tz = pytz.timezone('Europe/Berlin')
             ts = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
             gl_commands_costs.append([np.array([[self.measurements[0]]]), np.array([[self.measurements[1]]]),np.array([[commands]]), np.array([[storage_cost[0][1]]]),  np.array([[storage_out[0][2]]]),np.array([[storage_out[0][3]]]), self._name, ts])
-            """ For evaluation only"""  
-            sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'CommandsCosts.mat' ), {'CommandsCosts': gl_commands_costs})   
+            """ For evaluation only"""
+            sio.savemat((Init.path_res + '\\' + self._name + '\\' + 'CommandsCosts.mat' ), {'CommandsCosts': gl_commands_costs})
             """ Send commands """
             if self._name != 'Steam_humidifier' and  time_step-time_storage == Init.optimization_interval-Init.sync_rate:
                 self.SendCommands(commands)
+            print(commands)
             return commands
-     
+
     def SendCommands(self, commands):
         """ Send commands   """
-        
+
     def CalcXfromRH(self, relHum, T_hum, pressure=None):
         if pressure is None:
-            pressure = 101325            
+            pressure = 101325
         """Enhancement factors Water / Ice"""
         aw = 6.1121
         bw = 18.678
         cw = 257.14
         dw = 234.5
-    
+
         ai = 6.1115
         bi = 23.036
         ci = 279.82
         di = 333.7
-    
+
         pressure = pressure/100     # calculations require hekto pascal (equal mbar)
-        
+
         if T_hum > 0:
              EFw = 1 + 10**-4 *(7.2 + pressure * (0.0320 + 5.9*10**-6 * T_hum**2));
              f1wT = EFw * aw * math.exp((bw - T_hum/dw) * T_hum/(T_hum + cw));                       # saturation pressure from temperature
@@ -251,23 +255,23 @@ class Subsystem():
              absHum_measurements = ((18.015/28.963) * f1iDP /(pressure - f1iDP));
         return absHum_measurements
 
-        
+
     def CalcRHfromX(self, absHum, T_hum, pressure=None):
         if pressure is None:
-            pressure = 101325            
+            pressure = 101325
         """Enhancement factors Water / Ice"""
         aw = 6.1121
         bw = 18.678
         cw = 257.14
         dw = 234.5
-    
+
         ai = 6.1115
         bi = 23.036
         ci = 279.82
         di = 333.7
-    
+
         pressure = pressure/100     # calculations require hekto pascal (equal mbar)
-        
+
         if T_hum > 0:
              EFw = 1 + 10**-4 *(7.2 + pressure * (0.0320 + 5.9*10**-6 * T_hum**2));
              f1wT = EFw * aw * math.exp((bw - T_hum/dw) * T_hum/(T_hum + cw));                       # saturation pressure from temperature
@@ -277,7 +281,3 @@ class Subsystem():
              f1iT = EFi * ai * math.exp((bi - T_hum/di) * T_hum/(T_hum + ci));                       # saturation pressure from temperature
              relHum = (pressure*absHum*100/(absHum+(18.015/28.963)))/f1iT;
         return relHum
-    
-            
-        
-        
