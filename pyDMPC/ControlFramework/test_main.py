1aa2cd0dcae94a8af6f0e14d8938d552ca3ef967
Marc Baranski <mbaranski@eonerc.rwth-aachen.de>
2018-09-19 00:45:47 +0200

    Revert "* Simplyfy the test_main.py. This solves #10"
    This reverts commit 495eba86f3207e342b3970b45f2093a94d793a01.

--- pyDMPC/ControlFramework/test_main.py
+++ pyDMPC/ControlFramework/test_main.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 """
 This is a test module for new implementations.
+
 """
 
 import System
@@ -8,85 +10,85 @@ import Init
 import numpy as np
 import scipy.io as sio
 from pathos.multiprocessing import ProcessingPool as Pool
-import Objective_Function
 
 def main():
-    """Create a system and multiple subsystems"""
+    """ System and Subsystem Creation """
     AHU = System.System()
     subsystems = AHU.GenerateSubSys()
     #subsystems.reverse()  #only for BExMoC
 
-    """Variables storing (time) steps"""
+    counter = 0
     time_step = 0
     time_storage = 0
-    start = time.time()
-    counter = 0
-
-    """Variables storing commands"""
     storage_commands = np.zeros([5,1])
-
-    """There are currently three different options:
-    1. NC-OPT algorithm
-    2. NC-OPT algorithm using parallel computing
-    3. BExMoC algorithm
+    stor = []
     """
-
-    """The algorithms work with a discrete *time_step*. In each step, the current measurements are taken using the :func:`GetMeasurements' method. """
-    while time_step <= Init.sync_rate*Init.stop_time:
-        subsystems[0].GetMeasurements(AHU._measurements_IDs) #GetMeasurements
-
-        if Init.algorithm == 'NC_DMPC':
-
-            for k in range(5):
-                if Init.parallelization == False:
-                    for s in subsystems:
-                        print(s._name)
-                        commands = s.CalcDVvalues(time_step, time_storage,k)
-                else:
-                    def f(s):
-                        commands = s.CalcDVvalues(time_step, time_storage,k)
-                        return commands
-
-                    p = Pool(4)
-                    commands = p.map(f, [subsystems[0], subsystems[1], subsystems[2], subsystems[3], subsystems[4]])
-
-            print(commands)
-
-
-        elif Init.algorithm == 'BExMoC':
-
-            for s in subsystems:
-                print(s._name)
-
-                """The main calculations are carried out by invoking the :func:'CalcDVvalues' method. The BExMoC algorithm exchanges tables between the subsystems in a .mat format"""
-                commands = s.CalcDVvalues(time_step, time_storage,0)
-
-                print(s._name, commands)
-
-                (sio.savemat((Init.path_res + '\\' + s._name + '\\' +
-                'DV_lookUpTable' + str(counter) + '.mat' ),
-                {'DV_lookUpTable': s.lookUpTables[1]}))
-
-                (sio.savemat((Init.path_res + '\\' + s._name + '\\' +
-                'Costs_lookUpTable' + str(counter) + '.mat' ),
-                {'Cost_lookUpTable': s.lookUpTables[0]}))
-
-                (sio.savemat((Init.path_res + '\\' + s._name + '\\' +
-                'Output_Table' + str(counter) + '.mat' ),
-                {'Output_Table': s.lookUpTables[2]}))
-
-        if Init.realtime:
-            if time_step > 0:
-                time.sleep(max(Init.sync_rate-time.time()+start,0))
-                start = time.time()
+    # Non-parallel NC-DMPC
+    while counter < Init.max_num_iteration:
+        #Begin non-parallel NC-DMPC
+        subsystems[0].GetMeasurements(AHU._measurements_IDs)
+        start = time.time()
+        #for s in subsystems
+        s = subsystems[0]
+        print(s._name)
+        commands = s.CalcDVvalues(time_step, time_storage)
+        stor.append(commands)
+        end = time.time()
+        print(commands, 'time elapsed:', end-start)
+        counter += 1
         if time_step-time_storage >= Init.optimization_interval:
             time_storage = time_step
-        print(time_step)
         time_step += Init.sync_rate
-        counter += 1
-        print(time_step)
+        storage_commands = np.append(storage_commands, np.expand_dims(np.array(stor), axis = 1), axis = 1)
+        stor = []
+        sio.savemat((Init.path_res  + '\\' + 'Commands_NC_DMPC' + str(counter) + '.mat' ), {'Commands_NC_DMPC': storage_commands})
+        # End non-parallel NC-DMPC
 
+    #Parallel NC-DMPC
+    p = Pool(4)
+    while counter < Init.max_num_iteration:
+        start = time.time()
+        subsystems[0].GetMeasurements(AHU._measurements_IDs) #GetMeasurements as abstract method
+        # Begin Parallel Computing
+        def f(s):
+            commands = s.CalcDVvalues(time_step, time_storage)
+            return commands
+
+        commands = p.map(f, [subsystems[0], subsystems[1], subsystems[2], subsystems[3], subsystems[4]])
+        storage_commands = np.append(storage_commands, np.expand_dims(commands, axis = 1), axis = 1)
+        sio.savemat((Init.path_res  + '\\' + 'Commands_NC_DMPC' + str(counter) + '.mat' ), {'Commands_NC_DMPC': storage_commands})
+        print(commands)
+        end = time.time()
+        print('counter:', counter, 'time elapsed:', end-start)
+        counter += 1
+        if time_step-time_storage >= Init.optimization_interval:
+            time_storage = time_step
+        time_step += Init.sync_rate
+        end = time.time()
+        # End NC-DMPC Parallel
+"""
+    # Begin BExMoC
+    start = time.time()
+    while time_step <= Init.sync_rate*Init.stop_time:
+        subsystems[0].GetMeasurements(AHU._measurements_IDs) #GetMeasurements as abstract method
+        for s in subsystems:
+            print(s._name)
+            start = time.time()
+            commands = s.CalcDVvalues(time_step, time_storage)
+            end = time.time()
+            print(s._name, commands, 'time elapsed:', end-start)
+            sio.savemat((Init.path_res + '\\' + s._name + '\\' + 'DV_lookUpTable' + str(counter) + '.mat' ), {'DV_lookUpTable': s.lookUpTables[1]})
+            sio.savemat((Init.path_res + '\\' + s._name + '\\' + 'Costs_lookUpTable' + str(counter) + '.mat' ), {'Cost_lookUpTable': s.lookUpTables[0]})
+            sio.savemat((Init.path_res + '\\' + s._name + '\\' + 'Output_Table' + str(counter) + '.mat' ), {'Output_Table': s.lookUpTables[2]})
+        if time_step > 0:
+            time.sleep(10)
+        if time_step-time_storage >= Init.optimization_interval:
+            time_storage = time_step
+        time_step += Init.sync_rate
+    import Objective_Function
     Objective_Function.CloseDymola()
+ 
+    # End BExMoC
 
     print("Program successfully executed")
 
