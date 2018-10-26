2990123e4673c1b0d6fe226a9316bf885a6f7ea1
Marc Baranski <mbaranski@eonerc.rwth-aachen.de>
2018-10-26 10:43:51 +0200

    * Correct IDs in the Init file, #16.

--- pyDMPC/ControlFramework/Init.py
+++ pyDMPC/ControlFramework/Init.py
@@ -6,12 +6,12 @@ amount_consumer = 4
 amount_generator = 1
 
 """Data point IDs in the controlled system"""
-measurements_IDs = ['outdoorTemperature.y', 'outdoorHumidity.y', 'outgoingAirOutletTemperatureC.Celsius',
-                    'inOutlets.OutgoingAirRelHumid.phi', 'roomTemperature.y', 'roomHumidity.y',
-                    'heaterTemperatureC.Celsius', 'outdoorHumidity.y', 'preHeaterTemperatureC.Celsius',
-                    'coolerTemperatureC.Celsius', 'heaterTemperatureC.Celsius', 'highTemperatureCircuit.y',
-                    'coolingCircuit.y', 'highTemperatureCircuit.y', 'preHeater.returnTempInC.Celsius',
-                    'heater.returnTempInC.Celsius', 'hRCTemperatureC.Celsius','outgoingAirOutletTemperatureHumidity.phi','outgoingAirOutletTemperatureHumidity.phi']
+measurements_IDs = ['outdoorTemperatureOutput', 'outdoorHumidityOutput', 'outgoingAirOutletTemperatureCOutput',
+                    'outgoingAirOutletHumidityOutput', 'roomTemperatureOutput', 'roomHumidityOutput',
+                    'supplyAirTemperatureCOutput', 'supplyHumidityOutput', 'preHeaterTemperatureCOutput',
+                    'coolerTemperatureCOutput', 'heaterTemperatureCOutput', 'highTemperatureCircuitOutput',
+                    'coolingCircuitOutput', 'highTemperatureCircuitOutput', 'preHeaterReturnTemperatureOutput',
+                    'heaterReturnTemperatureOutput', 'outgoingAirOutletTemperatureCOutput','outgoingAirOutletHumidityOutput','outgoingAirOutletHumidityOutput']
 
 """ General algorithm settings """
 algorithm = 'BExMoC'   #choices: 'NC_DMPC', 'BExMoC'
@@ -20,7 +20,7 @@ realtime = True         #Choose True for a real-life experiment
 
 """ Settings for BExMoC algorithm """
 # So far: For all subsystems the same settings
-factors_BCs = [3, 0.01]              # order: BC1, BC2, ...
+factors_BCs = [3, 0.016]              # order: BC1, BC2, ...
 center_vals_BCs = [30, 0.001]
 amount_lower_vals = [2, 0]
 amount_upper_vals = [2, 1]
@@ -38,12 +38,12 @@ cost_gradient = 0
 obj_function = 'Monetary'   #choices: 'Exergy', 'Monetary'
 set_point = [30, 0.005]     #set points of the controlled variables
 tolerance = 1
-cost_factor = 0.0001
+cost_factor = 1
 
 """ Time and Interval Settings """
 sim_time_global = 10000          # -> not used yet
-sync_rate = 10*60                 # Synchronisation rate in seconds
-optimization_interval = 2*60    # After one interval the optimization is repeated
+sync_rate = 2*60                 # Synchronisation rate in seconds
+optimization_interval = 20*60    # After one interval the optimization is repeated
 prediction_horizon = 3600        #Common prediction horizon in seconds
 
 """ Directories and Modelica libraries """
@@ -122,6 +122,7 @@ IDs_initial_values= [] #for simulation
 cost_par = [] #for MassFlowRate
 valveSettings = [] #for FMU
 lenInitials = []
+IDs_inputs = []
 
 """ Subsystems """
 # Heat recovery system
@@ -136,9 +137,9 @@ Id_BC1.append(measurements_IDs[0])
 Id_BC2.append([measurements_IDs[1],measurements_IDs[0]])
 names_DVs.append('4120.L04_.AASYY17_NE-Ventil')
 output_vars.append(["supplyAirTemperature.T","supplyAirHumidity.phi"]) #NC_DMPC
-initial_names.append(["OutgoingHex.ele[1].mas.T","OutgoingHex.ele[2].mas.T","OutgoingHex.ele[3].mas.T","OutgoingHex.ele[4].mas.T",
-                      "IntakeHex.ele[1].mas.T","IntakeHex.ele[1].mas.T","IntakeHex.ele[1].mas.T","IntakeHex.ele[1].mas.T"])
-IDs_initial_values.append([measurements_IDs[2],measurements_IDs[2],measurements_IDs[2],measurements_IDs[2],measurements_IDs[16],measurements_IDs[16],measurements_IDs[16],measurements_IDs[16]])
+initial_names.append(["OutgoingHex.ele[1].mas.T","OutgoingHex.ele[2].mas.T","OutgoingHex.ele[3].mas.T","OutgoingHex.ele[4].mas.T","IntakeHex.ele[1].mas.T","IntakeHex.ele[1].mas.T","IntakeHex.ele[1].mas.T","IntakeHex.ele[1].mas.T"])
+IDs_initial_values.append(["inOutletsOutgoingHexele1masT","inOutletsOutgoingHexele2masT","inOutletsOutgoingHexele3masT","inOutletsOutgoingHexele4masT","inOutletsIntakeHexele1masT","inOutletsIntakeHexele2masT","inOutletsIntakeHexele3masT","inOutletsIntakeHexele4masT"])
+IDs_inputs.append([ 'outdoorHumidityOutput','outdoorTemperatureOutput'])
 lenInitials.append(8)
 cost_par.append('RecirculationPressure.ports[1].m_flow')
 valveSettings.append('valveHRS')
@@ -156,7 +157,8 @@ Id_BC2.append([measurements_IDs[1],measurements_IDs[0]])
 names_DVs.append('4120.L04_.AASYY17_NE-Ventil')
 output_vars.append(["supplyAirTemperature.T","supplyAirHumidity.phi"]) #NC_DMPC
 initial_names.append(["hex.ele[1].mas.T","hex.ele[2].mas.T","hex.ele[3].mas.T","hex.ele[4].mas.T"])
-IDs_initial_values.append([measurements_IDs[8],measurements_IDs[8],measurements_IDs[8],measurements_IDs[8]])
+IDs_initial_values.append(["preHeaterhexele1masT","preHeaterhexele2masT","preHeaterhexele3masT","preHeaterhexele4masT"])
+IDs_inputs.append(["hRCHumidityOutput","hRCTemperatureCOutput"])
 lenInitials.append(4)
 cost_par.append('val.port_1.m_flow')
 valveSettings.append('valvePreHeater')
@@ -174,7 +176,8 @@ Id_BC2.append([measurements_IDs[1],measurements_IDs[0]])
 names_DVs.append('4120.L04_.AASYY17_NE-Ventil')
 output_vars.append(["supplyAirTemperature.T","supplyAirHumidity.phi"]) #NC_DMPC
 initial_names.append(["hex.ele[1].mas.T","hex.ele[2].mas.T","hex.ele[3].mas.T","hex.ele[4].mas.T"])
-IDs_initial_values.append([measurements_IDs[9],measurements_IDs[9],measurements_IDs[9],measurements_IDs[9]])
+IDs_initial_values.append(["coolerhexele1masT","coolerhexele2masT","coolerhexele3masT","coolerhexele4masT"])
+IDs_inputs.append(["preHeaterHumidityOutput","preHeaterTemperatureCOutput"])
 lenInitials.append(4)
 cost_par.append('CoolerValve.port_b.m_flow')
 valveSettings.append('valveCooler')
@@ -192,7 +195,8 @@ Id_BC2.append([measurements_IDs[1],measurements_IDs[0]])
 names_DVs.append('4120.L04_.AASYY17_NE-Ventil')
 output_vars.append(["supplyAirTemperature.T","supplyAirHumidity.phi"]) #NC_DMPC
 initial_names.append(["hex.ele[1].mas.T","hex.ele[2].mas.T","hex.ele[3].mas.T","hex.ele[4].mas.T"])
-IDs_initial_values.append([measurements_IDs[10],measurements_IDs[10],measurements_IDs[10],measurements_IDs[10]])
+IDs_initial_values.append(["heaterhexele1masT","heaterhexele2masT","heaterhexele3masT","heaterhexele4masT"])
+IDs_inputs.append(["coolerHumidityOutput","coolerTemperatureCOutput"])
 lenInitials.append(4)
 cost_par.append('val.port_1.m_flow')
 valveSettings.append('valveHeater')
@@ -211,6 +215,7 @@ names_DVs.append("humidifierWSP1")
 output_vars.append(["supplyAirTemperature.T","supplyAirHumidity.phi"]) #NC_DMPC Humdidifier
 initial_names.append(None)
 IDs_initial_values.append(None)
+IDs_inputs.append(["heaterHumidityOutput","heaterTemperatureCOutput"])
 lenInitials.append(0)
 cost_par.append('product3.y')
 valveSettings.append('humidifierWSP1')
