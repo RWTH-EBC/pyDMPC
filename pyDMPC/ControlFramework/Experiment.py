b25a8da4e8deef45c45054477e8ea4b4036588d9
Marc Baranski <mbaranski@eonerc.rwth-aachen.de>
2019-06-02 15:02:40 +0200

    * add try and except, #49

--- pyDMPC/ControlFramework/Experiment.py
+++ pyDMPC/ControlFramework/Experiment.py
@@ -10,6 +10,7 @@ import time
 import Time
 
 def main():
+    
 
     # Generate a system for the experiment
     Time.Time.contr_sys_type = "Modelica"
@@ -21,14 +22,25 @@ def main():
     
     hall.initialize()
     
-    for cur_time in range(0, 86400 , min_samp_inter):
-        start_time = time.time()
-        hall.execute()
-        time.sleep(max(0, min_samp_inter - time.time() + start_time))
-        
-        
-    System.Bexmoc.close_mod()
-    System.Bexmoc.close_cont_sys()
-    print("Success")
+    try:
+        try:
+    
+            for cur_time in range(0, 86400 , min_samp_inter):
+                start_time = time.time()
+                hall.execute()
+                time.sleep(max(0, min_samp_inter - time.time() + start_time))
+                
+                
+            System.Bexmoc.close_mod()
+            System.Bexmoc.close_cont_sys()
+            print("Success")
+            
+        except Exception as e:
+            hall.terminate()
+            print(getattr(e, 'message', repr(e)))
+            
+    except KeyboardInterrupt:
+        hall.terminate()
+        print('Interrupted')
     
 if __name__=="__main__": main()
